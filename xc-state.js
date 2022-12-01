896549aae9a8f816f5f10ae1fcc7c6c095003374
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2022-11-30 23:14:17 -0500

    cross-chain state initialization

--- xc-state.js
+++ xc-state.js
@@ -1,12 +1,28 @@
+const assert = require("assert");
 const XChainContract = require("./xccontract");
 const addresses = require("./constants/contract-addresses.json");
+const { Tree } = require("holo-merkle-utils");
 
-// Global state tracker
+
+// NOTE: make sure depth is 14, perhaps put this in /constants
+const DEPTH = 14; 
+
+/* Keeps track of cross-chain Merkle tree state */
 class XChainState {
     constructor() {
         this.xchub = new XChainContract("Hub");
-        this.xcleaves = {}; // Format: { "networkName" : ["leaf0", "leaf1", ..."leafn"] }
+        this.xcleaves = {};  // Format: { "networkName" : ["leaf0", "leaf1", ..."leafn"] }
         this.xctrees = {}; // // Format: { "networkName" : MerkleTree }
+        this.xchub.getLeaves().then(result => {
+            assert(result, "Failed to retrieve Merkle leaves");
+            this.xcleaves = result;
+            Object.keys(this.xcleaves).forEach(networkName => (
+                this.xctrees[networkName] = Tree(
+                    DEPTH, 
+                    this.xcleaves[networkName].map(leaf=>leaf.toString())
+                )
+            ));
+        });
     }
     // async forceRefreshAll() {
     //     this.xcleaves = await this.xchub.getLeaves();
@@ -17,11 +33,13 @@ class XChainState {
     async refreshNetwork(networkName) {
         const numLeaves = this.xcleaves[networkName].length;
         const newLeaves = await this.xchub.contracts[networkName].getLeavesFrom(numLeaves);
+        // NOTE: re-entrancy if pushing into leaves fails, but I don't see a problem
         for (const leaf of newLeaves) {
             this.xctrees[networkName].insert(leaf);
             this.xcleaves[networkName].push(leaf);
         }
     }
-
     
 }
+
+new XChainState();
