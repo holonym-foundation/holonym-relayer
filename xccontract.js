c12b4c1a7712fa2918d52c3332dd6e04a53fb6a1
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2022-12-05 15:43:33 -0500

    initContracts can only be called onced

--- xccontract.js
+++ xccontract.js
@@ -3,16 +3,28 @@ const { ethers } = require("ethers")
 const { deployTestingContracts } = require("./scripts/deploy-testing-contracts.js");
 const abis = require("./constants/abis");
 let addresses;
+let addressesLoading;
+
+async function initAddresses (){
+    if(addresses) return;
+    
+    // This is run when initAddresses is called for the very first time
+    if(!addressesLoading) {
+        addressesLoading = true;
+        addresses = (process.env.HARDHAT_TESTING === "true") ? await deployTestingContracts() : require("./constants/contract-addresses.json");
+    }
+    // This loop is run when initAddresses is called twice so it doesn't try loading addresses twice
+    while(addressesLoading) {
+        await new Promise(resolve=>setTimeout(resolve, 500))
+    }
+    
+}
 
 const nets = process.env.NETWORKS // "mainnet" or "testnet"
 // Safely create a cross-chain contract wrapper, after addresses have loaded
 async function CreateXChainContract(...args) {
-    if(process.env.HARDHAT_TESTING === "true"){
-        addresses = await deployTestingContracts();
-    } else {
-        addresses = require("./constants/contract-addresses.json");
-    }
-    return new XChainContract(...args)
+    await initAddresses();
+    return new XChainContract(...args);
 }
 // if(nets === "hardhat" && !process.env.HARDHAT_TESTING) { console.error("WARNING: need to run with npx hardhat test") }
 // TODO : use DefaultProvider with backup providers, not just an AlchemyProvider
@@ -31,6 +43,7 @@ class XChainContract {
         // Populate providers & signers
         for ( const networkName of Object.keys(this.addresses) ) {
             const address = this.addresses[networkName];
+            if(process.env.HARDHAT_TESTING === "true") console.log("adding provider", ethers.provider)
             const provider = (process.env.HARDHAT_TESTING === "true") ? ethers.provider : new ethers.providers.AlchemyProvider(networkName, process.env.ALCHEMY_APIKEY);
             const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
             const contract = new ethers.Contract(address, this.abi, signer);
@@ -45,6 +58,7 @@ class XChainContract {
                 const responses = {};
                 for ( const networkName of Object.keys(this.contracts) ) {
                     const contract = this.contracts[networkName];
+                    console.log(this.contracts[networkName], "ahijllkjhlkjjh")
                     const result = await contract[functionName](...args);
                     responses[networkName] = result;
                 }
